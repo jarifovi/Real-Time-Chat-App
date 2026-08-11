d5850d540a6d3c146987120700d7bff70c00b710
jarifovi <jarifovi7@gmail.com>
2026-08-12 00:30:12 +0600

    Configure real Firebase Web API key and app ID for project real-time-chat-app-eae59

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,9 +3,6 @@ import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for project: real-time-chat-app-eae59
-///
-/// If using Web, Android, or iOS apps, paste your web API key and App ID
-/// from your Firebase Console (Project Settings > General).
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
@@ -33,44 +30,44 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDemoPlaceholderKeyForRealTimeChatApp',
-    appId: '1:1234567890:web:abcdef1234567890',
-    messagingSenderId: '1234567890',
+    apiKey: 'AIzaSyAEmKMDchdHzCUp3u7-fP0BQE-wp-opfak',
+    appId: '1:64531853621:web:b1d830b5e56d782f0adfe8',
+    messagingSenderId: '64531853621',
     projectId: 'real-time-chat-app-eae59',
     authDomain: 'real-time-chat-app-eae59.firebaseapp.com',
     storageBucket: 'real-time-chat-app-eae59.firebasestorage.app',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDemoPlaceholderKeyForRealTimeChatApp',
-    appId: '1:1234567890:android:abcdef1234567890',
-    messagingSenderId: '1234567890',
+    apiKey: 'AIzaSyAEmKMDchdHzCUp3u7-fP0BQE-wp-opfak',
+    appId: '1:64531853621:android:b1d830b5e56d782f0adfe8',
+    messagingSenderId: '64531853621',
     projectId: 'real-time-chat-app-eae59',
     storageBucket: 'real-time-chat-app-eae59.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDemoPlaceholderKeyForRealTimeChatApp',
-    appId: '1:1234567890:ios:abcdef1234567890',
-    messagingSenderId: '1234567890',
+    apiKey: 'AIzaSyAEmKMDchdHzCUp3u7-fP0BQE-wp-opfak',
+    appId: '1:64531853621:ios:b1d830b5e56d782f0adfe8',
+    messagingSenderId: '64531853621',
     projectId: 'real-time-chat-app-eae59',
     storageBucket: 'real-time-chat-app-eae59.firebasestorage.app',
     iosBundleId: 'com.example.realTimeChatApp',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDemoPlaceholderKeyForRealTimeChatApp',
-    appId: '1:1234567890:ios:abcdef1234567890',
-    messagingSenderId: '1234567890',
+    apiKey: 'AIzaSyAEmKMDchdHzCUp3u7-fP0BQE-wp-opfak',
+    appId: '1:64531853621:ios:b1d830b5e56d782f0adfe8',
+    messagingSenderId: '64531853621',
     projectId: 'real-time-chat-app-eae59',
     storageBucket: 'real-time-chat-app-eae59.firebasestorage.app',
     iosBundleId: 'com.example.realTimeChatApp',
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyDemoPlaceholderKeyForRealTimeChatApp',
-    appId: '1:1234567890:web:abcdef1234567890',
-    messagingSenderId: '1234567890',
+    apiKey: 'AIzaSyAEmKMDchdHzCUp3u7-fP0BQE-wp-opfak',
+    appId: '1:64531853621:web:b1d830b5e56d782f0adfe8',
+    messagingSenderId: '64531853621',
     projectId: 'real-time-chat-app-eae59',
     authDomain: 'real-time-chat-app-eae59.firebaseapp.com',
     storageBucket: 'real-time-chat-app-eae59.firebasestorage.app',
